a17de3a6db3873bc90f5f71eed8739570f2d9a93
himelhasan <himelhasan497@gmail.com>
2023-02-24 17:50:14 +0600

    react convention applied to remove inspection warnings

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -5,22 +5,25 @@ import frameSvg from "../html/img/icons/Frame.svg";
 import classType from "../html/img/icons/class.svg";
 import avatar from "../html/img/icons/avatar.svg";
 import DestinationTableRow from "../Components/DestinationTableRow";
+import { useSelector } from "react-redux";
 
 const Home = () => {
+  const alFlights = useSelector((state) => state);
+
   return (
-    <body>
+    <div>
       <section>
         {/* <!-- Input Data --> */}
-        <div class="mt-[160px] mx-4 md:mt-[160px] relative">
-          <div class="bg-white rounded-md max-w-6xl w-full mx-auto">
-            <form class="first-hero lws-inputform">
+        <div className="mt-[160px] mx-4 md:mt-[160px] relative">
+          <div className="bg-white rounded-md max-w-6xl w-full mx-auto">
+            <form className="first-hero lws-inputform">
               {/* <!-- From --> */}
-              <div class="des-from">
+              <div className="des-from">
                 <p>Destination From</p>
-                <div class="flex flex-row">
+                <div className="flex flex-row">
                   <img src={frameSvg} alt="" />
                   <select
-                    class="outline-none px-2 py-2 w-full"
+                    className="outline-none px-2 py-2 w-full"
                     name="from"
                     id="lws-from"
                     required
@@ -37,12 +40,12 @@ const Home = () => {
               </div>
 
               {/* <!-- To --> */}
-              <div class="des-from">
+              <div className="des-from">
                 <p>Destination To</p>
-                <div class="flex flex-row">
+                <div className="flex flex-row">
                   <img src={frameSvg} alt="" />
                   <select
-                    class="outline-none px-2 py-2 w-full"
+                    className="outline-none px-2 py-2 w-full"
                     name="to"
                     id="lws-to"
                     required
@@ -59,11 +62,11 @@ const Home = () => {
               </div>
 
               {/* <!-- Date --> */}
-              <div class="des-from">
+              <div className="des-from">
                 <p>Journey Date</p>
                 <input
                   type="date"
-                  class="outline-none px-2 py-2 w-full date"
+                  className="outline-none px-2 py-2 w-full date"
                   name="date"
                   id="lws-date"
                   required
@@ -71,12 +74,12 @@ const Home = () => {
               </div>
 
               {/* <!-- Guests --> */}
-              <div class="des-from">
+              <div className="des-from">
                 <p>Guests</p>
-                <div class="flex flex-row">
+                <div className="flex flex-row">
                   <img src={avatar} alt="" />
                   <select
-                    class="outline-none px-2 py-2 w-full"
+                    className="outline-none px-2 py-2 w-full"
                     name="guests"
                     id="lws-guests"
                     required
@@ -93,12 +96,12 @@ const Home = () => {
               </div>
 
               {/* <!-- Class --> */}
-              <div class="des-from !border-r-0">
+              <div className="des-from !border-r-0">
                 <p>Class</p>
-                <div class="flex flex-row">
+                <div className="flex flex-row">
                   <img src={classType} alt="" />
                   <select
-                    class="outline-none px-2 py-2 w-full"
+                    className="outline-none px-2 py-2 w-full"
                     name="ticketClass"
                     id="lws-ticketClass"
                     required
@@ -112,41 +115,41 @@ const Home = () => {
                 </div>
               </div>
 
-              <button class="addCity" type="submit" id="lws-addCity">
+              <button className="addCity" type="submit" id="lws-addCity">
                 <svg
                   width="15px"
                   height="15px"
                   fill="none"
                   viewBox="0 0 24 24"
-                  stroke-width="2"
+                  strokeWidth="2"
                   stroke="currentColor"
                 >
                   <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
                     d="M12 4.5v15m7.5-7.5h-15"
                   />
                 </svg>
-                <span class="text-sm">Book</span>
+                <span className="text-sm">Book</span>
               </button>
             </form>
           </div>
         </div>
 
         {/* <!-- Preview Data --> */}
-        <div class="table-container">
-          <table class="booking-table">
-            <thead class="bg-gray-100/50">
-              <tr class="text-black text-left">
+        <div className="table-container">
+          <table className="booking-table">
+            <thead className="bg-gray-100/50">
+              <tr className="text-black text-left">
                 <th>Destination From</th>
                 <th>Destination To</th>
-                <th class="text-center">Journey Date</th>
-                <th class="text-center">Guests</th>
-                <th class="text-center">Class</th>
-                <th class="text-center">Delete</th>
+                <th className="text-center">Journey Date</th>
+                <th className="text-center">Guests</th>
+                <th className="text-center">Class</th>
+                <th className="text-center">Delete</th>
               </tr>
             </thead>
-            <tbody class="divide-y divide-gray-300/20" id="lws-previewBooked">
+            <tbody className="divide-y divide-gray-300/20" id="lws-previewBooked">
               <DestinationTableRow />
               <DestinationTableRow />
               <DestinationTableRow />
@@ -154,7 +157,7 @@ const Home = () => {
           </table>
         </div>
       </section>
-    </body>
+    </div>
   );
 };
 
