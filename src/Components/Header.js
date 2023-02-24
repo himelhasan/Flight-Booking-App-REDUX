17142f93536bc50acdd50a85240f51e59b568bcc
himelhasan <himelhasan497@gmail.com>
2023-02-24 16:11:51 +0600

    template setup in react from html, componenets created

---
+++ src/Components/Header.js
@@ -0,0 +1,22 @@
+import React from "react";
+import logo from "../html/img/lws-logo.svg";
+
+const Header = () => {
+  return (
+    <div>
+      <header id="header">
+        <div class="container">
+          <img src={logo} alt="logo" class="logo" />
+          <div class="flex items-center">
+            <a class="text-white min-w-[50px] font-medium" href="#">
+              Home
+            </a>
+            <button class="log-btn btn">Login</button>
+          </div>
+        </div>
+      </header>
+    </div>
+  );
+};
+
+export default Header;
